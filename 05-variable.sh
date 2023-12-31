c49bbd278548827005bd2fae64e03ac9ab1c78d2
theppasiva <theppasiva@gmail.com>
2024-01-01 02:09:00 +0530

    added fifth script

--- 05-variable.sh
+++ 05-variable.sh
@@ -3,6 +3,11 @@
 echo "please enter your username"
 
 read -s USERNAME
-PASSWORD=$2
 
 echo "username is:$USERNAME"
+
+echo "please enter your password"
+
+read -s PASSWORD
+
+echo "username is:$PASSWORD"
