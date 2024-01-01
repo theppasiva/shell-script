47e8de2e201af9195881481159b7c44b584a855e
theppasiva <theppasiva@gmail.com>
2024-01-01 12:39:48 +0530

    added 10th script

---
+++ 11-logs.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+ID=$(id -u)
+
+echo "Script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "EROOR::$2.... Failed"
+        exit 1
+    else
+        echo "$2....Success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing MYSQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
