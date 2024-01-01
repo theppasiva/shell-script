a0cf9e5a625de5e3c403f71f491507c1863990df
theppasiva <theppasiva@gmail.com>
2024-01-01 11:57:41 +0530

    added 9th script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -19,3 +19,13 @@ then
 else
     echo "installing MYSQL is success"
 fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "EROOR::Installing GIT is failed"
+    exit 1
+else
+    echo "installing GIT is success"
+fi
