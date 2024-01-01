4cd9f6549d5c697ba69ada6acd25061a2fbb6332
theppasiva <theppasiva@gmail.com>
2024-01-01 16:50:11 +0530

    added 13th script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -11,7 +11,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE=(){
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 .... $R FAILED$N"
