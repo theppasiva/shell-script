16f7002ead4d588418d8f90715d5d2020fe6d5c6
theppasiva <theppasiva@gmail.com>
2024-01-01 12:08:27 +0530

    added 10th script

---
+++ 10-functions.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+ID=$(id -u)
+
+validate(){
+    if [ $? -ne 0 ]
+    then
+        echo "EROOR::Installing MYSQL is failed"
+        exit 1
+    else
+        echo "installing MYSQL is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR: please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+yum install mysql -y
+
+validate
+
+yum install git -y
+
+validate
