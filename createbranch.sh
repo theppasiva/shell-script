88e63cdcebf0ed74f9e27f5df199f993e3c9e252
theppasiva <theppasiva@gmail.com>
2024-04-11 09:43:55 +0530

    branch.sh

---
+++ createbranch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# List of repository URLs
+repos=(
+    "https://github.com/theppasiva/shell-script.git"
+    "https://github.com/theppasiva/roboshop-shell.git"
+    # Add more repository URLs as needed
+)
+
+# New branch name
+new_branch="feature"
+
+for repo in "${repos[@]}"; do
+    # Clone the repository
+    git clone "$repo" temp_repo
+
+    # Enter the cloned repository directory
+    cd temp_repo || exit
+
+    # Create a new branch
+    git checkout -b "$new_branch"
+
+    # Push the new branch to the remote repository
+    git push origin "$new_branch"
+
+    # Go back to the parent directory
+    cd ..
+
+    # Remove the temporary cloned repository directory
+    rm -rf temp_repo
+done
