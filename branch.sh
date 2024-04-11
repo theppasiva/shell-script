a85cd98094357677df977faea3a48bc65ae8a320
theppasiva <theppasiva@gmail.com>
2024-04-11 09:22:18 +0530

    branch.sh

---
+++ branch.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+ echo "name of the branch ?"
+
+ read branch
+
+ echo "Crreate $branch from current branch(c) or from Master(m)?"
+ read -p "current(c) / Master(m) ?" cm
+
+ if [ $cm == 'c' ]; then
+    git checkout -b $branch
+elseif [ $cm == 'm' ]; then
+    git checkout master && git pull origin  master && git checkout -b $branch
+else
+    echo " branch is :$branch"
+fi
